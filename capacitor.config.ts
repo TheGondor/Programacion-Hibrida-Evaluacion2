039bdeca269ce500e46bce547dbbf65e1d20d61f
Gustavo Muñoz Muñozç <gusmunozmu@gmail.com>
2026-08-30 19:36:27 -0400

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'evaluacion.v2',
+  appName: 'evaluacion-2',
+  webDir: 'www'
+};
+
+export default config;
